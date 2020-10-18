bd86d25706da968ecfb7ae6a20be35e3c3d3eecd
JAVI <ortiz.javier10@gmail.com>
2020-10-18 16:35:58 -0400

    nav index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Nav from './components/Nav';
+
 
 function App() {
+  const tabs = ['About', 'Contact', 'Portfolio', 'Resume'];
+  const [currentPage, handlePageChange] = useState('About');
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'About': return <About />;
+      case 'Contact': return <Contact />;
+      case 'Portfolio': return <Portfolio />;
+      case 'Resume': return <Resume />;
+      default: return <About />;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav tabs={tabs} currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main>
+        {renderPage()}
+      </main>
     </div>
   );
 }
