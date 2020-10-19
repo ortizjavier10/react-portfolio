7dc34c09de2a9775d446ab53e8a1165fc6a0f312
JAVI <ortiz.javier10@gmail.com>
2020-10-19 19:46:26 -0400

    Updates

---
+++ precache-manifest.ca0118ec4e80204e3f0ee8faadc31209.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdffc4e27148c02510b80b7a8d60598",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "04e242673253d8b349a9",
+    "url": "/react-portfolio/static/css/main.34801c72.chunk.css"
+  },
+  {
+    "revision": "54c690d13fe64bf82aa3",
+    "url": "/react-portfolio/static/js/2.489c5252.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-portfolio/static/js/2.489c5252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e242673253d8b349a9",
+    "url": "/react-portfolio/static/js/main.39811bcf.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "560a42466c04d6e6355084f16a05da04",
+    "url": "/react-portfolio/static/media/background.560a4246.jpg"
+  },
+  {
+    "revision": "1f4ddb298542e99da86a86f097ca6904",
+    "url": "/react-portfolio/static/media/budget-tracker.1f4ddb29.JPG"
+  },
+  {
+    "revision": "cf78973c4b6ba97796aa2dd1bc5b5bea",
+    "url": "/react-portfolio/static/media/email.cf78973c.svg"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/react-portfolio/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "c7fc1d64d151e3228a7ec0c6d4ba4bac",
+    "url": "/react-portfolio/static/media/horiseon.c7fc1d64.JPG"
+  },
+  {
+    "revision": "784f200c576a53d5968376b3965ce240",
+    "url": "/react-portfolio/static/media/javier.784f200c.jpg"
+  },
+  {
+    "revision": "20177402a1e05f0de7322149631cefeb",
+    "url": "/react-portfolio/static/media/link.20177402.svg"
+  },
+  {
+    "revision": "d46b60a67723134e6bc2c55bb6bb6d51",
+    "url": "/react-portfolio/static/media/minnow.d46b60a6.png"
+  },
+  {
+    "revision": "884b7e2572b955b098c0879a2d7094a4",
+    "url": "/react-portfolio/static/media/project-gemini.884b7e25.PNG"
+  },
+  {
+    "revision": "0beeb2608d1df00b756f66bd82a904fc",
+    "url": "/react-portfolio/static/media/runbuddy.0beeb260.JPG"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/react-portfolio/static/media/twitter.a57ea219.svg"
+  }
+]);
